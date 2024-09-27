4cad2e8a36accd232bffcf7e24f2ff4d61311af0
John Doe <github_cloudlabsuser_2144@cloudlabsaiuser.com>
2024-09-27 00:32:35 +0000

    Changes

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,35 +11,33 @@ public class UserController : Controller
         // GET: User
         public ActionResult Index()
         {
-            // Implement the Index method here
+           return View("~/Views/User/Index.cshtml", userlist); // Ensure the method returns a view with the user list
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+           return View("~/Views/User/Details.cshtml", userlist[id]); // Ensure the method returns a view with the user details
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View("~/Views/User/Create.cshtml"); // Ensure the method returns a view to create a new user
         }
 
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+          return View("~/Views/User/Create.cshtml", user); // Ensure the method returns a view to create a new user
         }
 
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+           return View("~/Views/User/Edit.cshtml", userlist[id]); // Ensure the method returns a view to edit a user}
         }
-
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
@@ -49,12 +47,16 @@ public class UserController : Controller
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            // Implement the Edit method (POST) here
+
+            return View("~/Views/User/Edit.cshtml", user); // Ensure the method returns a view to edit a user
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            // Implement the Delete method (GET) here
+            return View("~/Views/User/Delete.cshtml", userlist[id]); // Ensure the method returns a view to delete a user
         }
 
         // POST: User/Delete/5
@@ -62,5 +64,6 @@ public class UserController : Controller
         public ActionResult Delete(int id, IFormCollection collection)
         {
             // Implement the Delete method (POST) here
+            return View("~/Views/User/Delete.cshtml", userlist[id]); // Ensure the method returns a view to delete a user
         }
 }
--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f92c0cf5e7f299b649d4db5ff4b6c34345bf0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
